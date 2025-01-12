81d7ac3b8daa40c574375455e37acf38e73bca50
Joseph Brooksbank <joseph.brooksbank@navex.com>
2025-01-11 20:23:17 -0600

    get webserver online
    also stop tracking a bunch of build artifacts

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,35 @@
 #include <WiFi.h>
+#include <WebServer.h>
 #include <HTTPClient.h>
 #include <math.h>
 #include <Arduino.h>
-// #include<ArduinoJson.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include "index_html.h"
+
 
 #define BAUD 115200
-// put function declarations here:
-
-
 static const unsigned short ONBOARD_LED = 2;
 static const unsigned short C_LED = 6;
 static const unsigned short W_LED = 7;
 static const unsigned short ONBOARD_BUTTON = 0;
 static const unsigned short ONBOARD_TOUCH = 4;
 
+AsyncWebServer server(80);
+
+String processor(const String& var) 
+{
+  if (var == "test") {
+    return "changed!";
+  }
+
+  return var;
+}
+
 
 void setup()
 {
-  Serial0.begin(115200);
+  Serial.begin(115200);
   delay(1000);
   // put your setup code here, to run once:
 
@@ -25,19 +37,23 @@ void setup()
   pinMode(ONBOARD_LED, OUTPUT);
   
   WiFi.begin("generic network name", "MirrorWindowWall");
-  Serial0.println("\nConnecting...");
-
+  Serial.println("\nConnecting...");
 
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial0.print(".");
+    Serial.print(".");
   }
 
-  Serial0.println("\nConnected to Wifi!");
+  Serial.println("\nConnected to Wifi!");
 
-  Serial0.print("Local IP: ");
-  Serial0.print(WiFi.localIP());
+  Serial.print("Local IP: ");
+  Serial.print(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.begin();
 
 }
 
